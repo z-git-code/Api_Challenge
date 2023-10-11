a50083ee9db658bc56dede081db9e1fe0c13a315
z-git-code <guoliz2022@gmail.com>
2023-10-11 09:01:43 -0500

    adding files

---
+++ src/main/java/com/headjava/springboot/SpringbootAppApplication.java
@@ -0,0 +1,15 @@
+package com.headjava.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppApplication.class, args);
+	}
+
+
+
+}
